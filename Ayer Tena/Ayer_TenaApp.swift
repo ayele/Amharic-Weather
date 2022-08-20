28f1c054f2d7e969dd4b5e6dfebc26b441e0b960
amanuelayele <amanuelayele@gmail.com>
2022-08-20 11:18:46 -0500

    Initial Commit

---
+++ Ayer Tena/Ayer_TenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ayer_TenaApp.swift
+//  Ayer Tena
+//
+//  Created by Amanuel Ayele on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Ayer_TenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
