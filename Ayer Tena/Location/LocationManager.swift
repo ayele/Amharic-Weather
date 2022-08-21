43d64805312dd31217254352dab27a2a27e089a4
amanuelayele <amanuelayele@gmail.com>
2022-08-21 15:05:04 -0500

    implement location manager class

--- Ayer Tena/Location/LocationManager.swift
+++ Ayer Tena/Location/LocationManager.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    @Published private(set) var location: CLLocationCoordinate2D?
+    @Published private(set) var isLoading = false
+    
+    private let manager = CLLocationManager()
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation() {
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error getting location", error)
+        isLoading = false
+    }
+}
