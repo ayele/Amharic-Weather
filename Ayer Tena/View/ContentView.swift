44495e26b503ad2d9500e37e24d10e403f831c33
amanuelayele <amanuelayele@gmail.com>
2022-08-21 15:54:40 -0500

    implement content view display logic

--- Ayer Tena/View/ContentView.swift
+++ Ayer Tena/View/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    @StateObject var contentVM = ContentViewModel(service: WeatherService())
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Group {
+            if let location = locationManager.location {
+                if let weather = contentVM.weather {
+                    WeatherView(weather: weather)
+                } else {
+                    LoadingView()
+                        .task {
+                            await contentVM.getWeather(longitude: location.longitude, latitude: location.latitude)
+                        }
+                }
+            } else {
+                if locationManager.isLoading {
+                    LoadingView()
+                } else {
+                    WelcomeView()
+                        .environmentObject(locationManager)
+                }
+            }
+        }
     }
 }
 
