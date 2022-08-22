36e52b63dc918f97a00b375efcd382116f7687c5
amanuelayele <amanuelayele@gmail.com>
2022-08-22 17:01:38 -0500

    add extension to round double values

---
+++ Ayer Tena/Extensions/Double+Round.swift
@@ -0,0 +1,14 @@
+//
+//  Double+Round.swift
+//  Ayer Tena
+//
+//  Created by Amanuel Ayele on 8/22/22.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        return String(format: "%.0f", self)
+    }
+}
