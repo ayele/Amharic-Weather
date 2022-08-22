1db66b67d1c07f54e09cde8c5a6f9f4fa8566ed2
amanuelayele <amanuelayele@gmail.com>
2022-08-22 16:14:38 -0500

    implement api call to get weather data

--- Ayer Tena/Service/WeatherService.swift
+++ Ayer Tena/Service/WeatherService.swift
@@ -6,7 +6,27 @@
 //
 
 import Foundation
+import CoreLocation
 
 class WeatherService {
     
+    func getWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> Weather {
+        let apiKey = "4429f6b5fa654eac8cdaa21a45a49e47"
+        let baseURL = "https://api.openweathermap.org"
+        let url = "\(baseURL)/data/3.0/onecall?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&exclude=minutely&units=imperial"
+        
+        guard let url = URL(string: url) else { fatalError("Missing URL") }
+        
+        let urlRequest = URLRequest(url: url)
+        let (data, response) = try await URLSession.shared.data(for: urlRequest)
+        
+        let statusCode = (response as? HTTPURLResponse)?.statusCode
+        print("GET status code: \(statusCode ?? 000)")
+
+        guard statusCode == 200 else {
+            fatalError("Error fetching weather data")
+        }
+        
+        return try JSONDecoder().decode(Weather.self, from: data)
+    }
 }
