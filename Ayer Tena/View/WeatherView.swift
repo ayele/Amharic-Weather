331262a47403e1f8083603363691c9344d4ab995
amanuelayele <amanuelayele@gmail.com>
2022-08-23 11:38:41 -0500

    extract weather view subviews

--- Ayer Tena/View/WeatherView.swift
+++ Ayer Tena/View/WeatherView.swift
@@ -13,56 +13,11 @@ struct WeatherView: View {
     var body: some View {
         ScrollView(showsIndicators: false) {
             VStack {
-                VStack(spacing: -5) {
-                    Text("Middleton").font(.largeTitle)
-                    Text("\(weather.current.temperature.roundDouble())")
-                        .font(.system(size: 90))
-                    VStack(spacing: 5) {
-                        Text("\(weather.current.condition[0].description)").font(.title3)
-                        HStack {
-                            Text("H:80°").font(.title3)
-                            Text("L:58°").font(.title3)
-                        }
-                    }
-                }
-                .padding(50)
+                CurrentView(weather: weather)
                 
-                // Hourly
                 VStack(spacing: 20) {
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack(spacing: 20) {
-                            // We only need 24 hrs of hourly
-                            ForEach(weather.hourly[..<25]) { hourly in
-                                VStack(spacing: 15) {
-                                    Text("\(hourly.time.formattedHour())")
-                                    Image(systemName: "\(hourly.condition[0].icon.imageName)")
-                                    Text("\(hourly.temperature.roundDouble())°")
-                                }
-                            }
-                        }
-                    }
-                    .padding()
-                    .overlay { RoundedRectangle(cornerRadius: 15).stroke(.gray, lineWidth: 1) }
-                    
-                    // Daily
-                    VStack(spacing: 15) {
-                        ForEach(weather.daily) { daily in
-                            VStack {
-                                HStack {
-                                    Text("\(daily.time.formattedDay())")
-                                        .frame(width: 100, alignment: .leading)
-                                    Spacer()
-                                    Image(systemName: "\(daily.condition[0].icon.imageName)")
-                                    Spacer()
-                                    Text("\(daily.temperature.low.roundDouble())° - \(daily.temperature.high.roundDouble())°")
-                                        .frame(width: 100, alignment: .trailing)
-                                }
-                                Divider()
-                            }
-                        }
-                    }
-                    .padding()
-                    .overlay { RoundedRectangle(cornerRadius: 15).stroke(.gray, lineWidth: 1) }
+                    HourlyView(weather: weather)
+                    DailyView(weather: weather)
                 }
             }
             .padding()
@@ -73,5 +28,71 @@ struct WeatherView: View {
 struct WeatherView_Previews: PreviewProvider {
     static var previews: some View {
         WeatherView(weather: Weather.sampleData)
+    }
+}
+
+struct CurrentView: View {
+    let weather: Weather
+    
+    var body: some View {
+        VStack(spacing: -5) {
+            Text("Middleton").font(.largeTitle)
+            Text("\(weather.current.temperature.roundDouble())")
+                .font(.system(size: 90))
+            VStack(spacing: 5) {
+                Text("\(weather.current.condition[0].description)").font(.title3)
+                HStack {
+                    Text("H:80°").font(.title3)
+                    Text("L:58°").font(.title3)
+                }
+            }
+        }
+        .padding(50)
+    }
+}
+
+struct HourlyView: View {
+    let weather: Weather
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 20) {
+                // We only need 24 hrs of hourly
+                ForEach(weather.hourly) { hourly in
+                    VStack(spacing: 15) {
+                        Text("\(hourly.time.formattedHour())")
+                        Image(systemName: "\(hourly.condition[0].icon.imageName)")
+                        Text("\(hourly.temperature.roundDouble())°")
+                    }
+                }
+            }
+        }
+        .padding()
+        .overlay { RoundedRectangle(cornerRadius: 15).stroke(.gray, lineWidth: 1) }
+    }
+}
+
+struct DailyView: View {
+    let weather: Weather
+    
+    var body: some View {
+        VStack(spacing: 15) {
+            ForEach(weather.daily) { daily in
+                VStack {
+                    HStack {
+                        Text("\(daily.time.formattedDay())")
+                            .frame(width: 100, alignment: .leading)
+                        Spacer()
+                        Image(systemName: "\(daily.condition[0].icon.imageName)")
+                        Spacer()
+                        Text("\(daily.temperature.low.roundDouble())° - \(daily.temperature.high.roundDouble())°")
+                            .frame(width: 100, alignment: .trailing)
+                    }
+                    Divider()
+                }
+            }
+        }
+        .padding()
+        .overlay { RoundedRectangle(cornerRadius: 15).stroke(.gray, lineWidth: 1) }
     }
 }
