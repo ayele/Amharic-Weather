6428cc56f873244b0aef74c4920f9e5b05f11c88
amanuelayele <amanuelayele@gmail.com>
2022-08-24 15:33:32 -0500

    change weather loading and displaying logic

--- Ayer Tena/View/ContentView.swift
+++ Ayer Tena/View/ContentView.swift
@@ -14,14 +14,8 @@ struct ContentView: View {
     var body: some View {
         Group {
             if let location = locationManager.location {
-                if let weather = contentVM.weather {
-                    WeatherView(weather: weather)
-                } else {
-                    LoadingView()
-                        .task {
-                            await contentVM.getWeather(longitude: location.longitude, latitude: location.latitude)
-                        }
-                }
+                WeatherView(weatherVM: WeatherViewModel(location: location, service: WeatherService()))
+                
             } else {
                 if locationManager.isLoading {
                     LoadingView()
