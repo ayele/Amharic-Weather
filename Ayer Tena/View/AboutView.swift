43d4d6cead452f50b95f772d07d6cc27b862d07e
amanuelayele <amanuelayele@gmail.com>
2022-10-07 17:42:26 -0500

    update the about page

--- Ayer Tena/View/AboutView.swift
+++ Ayer Tena/View/AboutView.swift
@@ -10,14 +10,18 @@ import SwiftUI
 struct AboutView: View {
     var body: some View {
         VStack(spacing: 20) {
-            Text("Amet tellus cras adipiscing enim eu turpis egestas. Amet cursus sit amet dictum sit amet. Amet nisl purus in mollis. Non enim praesent elementum facilisis leo vel fringilla. Massa sed elementum tempus egestas sed sed. Felis eget nunc lobortis mattis aliquam faucibus.")
+            Image("Logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 70)
+                .padding()
             
-            Text("Amet tellus cras adipiscing enim eu turpis egestas. Amet cursus sit amet dictum sit amet. Amet nisl purus in mollis. Non enim praesent elementum facilisis leo vel fringilla. Massa sed elementum tempus egestas sed sed. Felis eget nunc lobortis mattis aliquam faucibus.")
+            Text("Amharic Weather is a weather app in አማርኛ. It was created by an independant software developer. Currently the app is only avialble in the US and on iOS only (Android maybe in the future). I came up with the idea when I was looking for a project to improve my programming skills. \n\nThe app gets its data from Apple Weather Service so the accuracy is identical to the native Apple Weather app")
             
             Spacer()
         }
-        .navigationTitle("About")
         .padding()
+        .navigationTitle("About")
     }
 }
 
