1a74d2d5c7d1103a5c70b33b1b136998328c744a
amanuelayele <amanuelayele@gmail.com>
2022-10-14 14:39:36 -0500

    refactor AboutView, add "buy me a coffee" button

--- Amharic Weather/View/Setting/AboutView.swift
+++ Amharic Weather/View/Setting/AboutView.swift
@@ -9,14 +9,29 @@ import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        VStack(spacing: 20) {
-            Image("Logo")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 70)
-                .padding()
+        VStack(alignment: .leading, spacing: 20) {
+            VStack(spacing: 15) {
+                Image("Lightning")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text("Amharic Weather is a weather app in አማርኛ. It was created by an independant software developer. Currently the app is only avialble in the US and on iOS only (Android maybe in the future). I came up with the idea when I was looking for a project to improve my programming skills. \n\nThe app gets its data from Apple Weather Service so the accuracy is identical to the native Apple Weather app")
+                    .font(.callout)
+            }
             
-            Text("Amharic Weather is a weather app in አማርኛ. It was created by an independant software developer. Currently the app is only avialble in the US and on iOS only (Android maybe in the future). I came up with the idea when I was looking for a project to improve my programming skills. \n\nThe app gets its data from Apple Weather Service so the accuracy is identical to the native Apple Weather app")
+            Button {
+                
+            } label: {
+                HStack {
+                    Text("Buy me a coffee")
+                    Image(systemName: "cup.and.saucer.fill")
+                }
+                .padding()
+                .overlay {
+                    RoundedRectangle(cornerRadius: 12).stroke(lineWidth: 1)
+                }
+            }
+            .buttonStyle(.plain)
             
             Spacer()
         }
@@ -27,9 +42,19 @@ struct AboutView: View {
 
 struct AboutView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationStack {
-            AboutView()
-                .navigationBarTitleDisplayMode(.inline)
+        
+        Group {
+            NavigationStack {
+                AboutView()
+                    .previewDisplayName("Light")
+            }
+            
+            NavigationStack {
+                AboutView()
+                    .preferredColorScheme(.dark)
+                    .previewDisplayName("Dark")
+            }
         }
+        .previewLayout(.sizeThatFits)
     }
 }
